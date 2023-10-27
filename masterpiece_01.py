69c0c3350ba8f9f5ded7a065a50de4932b72c598
Manuel Selch <manuel@selch.de>
2023-10-27 15:26:22 +0200

    masterpiece image terminal generator

--- masterpiece_01.py
+++ masterpiece_01.py
@@ -1,35 +1,38 @@
-img = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+limit = 10
 
-newImg = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
+img = [[1 for x in range(limit)] for y in range(limit)] 
 
-xLimit = 3
-yLimit = 3
+newImg = [[0 for x in range(limit)] for y in range(limit)] 
+
+count = 1
+for y in range(limit):
+    for x in range(limit):
+        count += 1
+        img[y][x] = count
+
 
 def getNewX(x):
-    newX = x*2
-    newX %= xLimit 
+    newX = x*3
+    newX %= limit 
     return newX
 def getNewY(y):
-    newY = y*2
-    newY %= yLimit 
+    newY = y*3
+    newY %= limit 
     return newY
 
 
 
-for y in range(3):
-    for x in range(3):
+for y in range(limit):
+    for x in range(limit):
         newX = getNewX(x)
         newY = getNewY(y)
         newImg[newY][newX] = img[y][x]
         print(str(x)+"|"+str(y) + " -> " + str(newX) + "|" + str(newY))
 
-for row in newImg:
+print("---")
+
+for y in range(limit):
+    row = ""
+    for x in range(limit):
+        row += "\033["+str(newImg[y][x])+"m" + str(newImg[y][x])
     print(row)
